a7e4d5e8fe641a157ff33926dfc2086744b0452d
Luke Jones <nnblc565@gmail.com>
2021-03-03 12:24:34 +1030

    Forogt to comment the SetCamera function in carbody so it was missed by boxygen

--- Project2D/CarBody.h
+++ Project2D/CarBody.h
@@ -12,7 +12,10 @@ public:
 	CarBody(PhysicsScene* scene, unsigned int layer, glm::vec2 position, glm::vec2 bodyExtents, glm::vec2 frontWheel, glm::vec2 backWheel, float mass, glm::vec4 bodyColour, glm::vec4 wheelColour, float wheelMass, float wheelRadius, float wheelAngAccel, float wheelAngVelLimit, float linearDrag, float angularDrag, float wheelElasticity);
 	//Since we add the wheels to the scenes actors, we don't have to deal with cleaning them up :D
 	~CarBody();
-
+	/// <summary>
+	/// Gives the car a reference to the camera
+	/// </summary>
+	/// <param name="renderer">A pointer to the camera</param>
 	void SetCamera(aie::Renderer2D* renderer) { m_renderer = renderer; }
 
 	virtual void FixedUpdate(glm::vec2 gravity, float timeStep);
